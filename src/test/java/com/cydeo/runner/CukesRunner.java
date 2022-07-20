995240acc14211423f39af2f34141c9b87021445
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-07-20 11:37:03 -0400

    Avengers

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html", //html report
+        plugin = {"html:target/cucumber-report.html", //html REPORT
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json"}, // cucumber report
+                "json:target/cucumber.json"}, //  cucumber report
         features = "src/test/resources/features" ,
         glue = "com/cydeo/steps",
          dryRun = false,
